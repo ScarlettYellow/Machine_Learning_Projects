254c842462d2843505ba3fd65dd4c4472137e33e
Scarlett Huang <scarlett.huang.hust@foxmail.com>
2018-03-01 11:08:39 +0800

    Add files via upload

---
+++ IMDB/IMDB.py
@@ -0,0 +1,5 @@
+import matplotlib inline
+import pandas as pd 
+import numpy as np  
+
+
---
+++ breast_cancer/cancer.py
@@ -0,0 +1,5 @@
+
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
